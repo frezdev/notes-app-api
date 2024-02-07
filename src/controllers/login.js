4e5be582d021e8f38ceca832b161c1783ace5169
Carlos Florez <84242409+frezdev@users.noreply.github.com>
2024-02-07 08:26:45 -0500

    Update login.js
    Add try/catch for error management

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -8,29 +8,33 @@ loginRouter.post('/', async (request, response) => {
   const { body } = request;
   const { username, password } = body;
 
-  const user = await User.findOne({ username });
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(password, user.passwordHash);
-
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'Invalid username or password'
+  try {
+    const user = await User.findOne({ username });
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash);
+  
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: 'Invalid username or password'
+      });
+    }
+  
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    };
+  
+    const token = jwt.sign(userForToken, SECRET);
+  
+    response.send({
+      name: user.name,
+      username: user.username,
+      token,
     });
+  } catch (error) {
+    response.status(500).send({message: "Ha ocurrido un error inesperado"})
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id
-  };
-
-  const token = jwt.sign(userForToken, SECRET);
-
-  response.send({
-    name: user.name,
-    username: user.username,
-    token,
-  });
 });
 
 module.exports = loginRouter;
